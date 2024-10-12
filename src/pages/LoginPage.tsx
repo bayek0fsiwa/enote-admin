33ca3df2ce5841ddcc8d0c3849a4b89d3634d0b1
bayek0fsiwa <imvikas@tutanota.com>
2024-10-12 15:12:44 +0530

    persist jwt localstore

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -14,17 +14,19 @@ import { useRef } from "react"
 import { useMutation } from "@tanstack/react-query"
 import { login } from "@/http/api"
 import { LoaderPinwheel } from "lucide-react"
+import useTokenStore from "@/store"
 
 const LoginPage = () => {
 
   const navigate = useNavigate();
-
+  const setToken = useTokenStore((state) => state.setToken);
   const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
 
   const mutation = useMutation({
     mutationFn: login,
-    onSuccess: () => {
+    onSuccess: ({ data }) => {
+      setToken(data.accessToken)
       navigate("/dashboard/home");
     },
   })
